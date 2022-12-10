b7b3e77e7cdbfdc6c1a9e8bcc1d859599fcadb04
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-12-11 01:28:37 +0200

    Update style.css

--- src/index.js
+++ src/index.js
@@ -90,9 +90,11 @@ function onFormSubmit(evt) {
 
 
 function creatMarkup(arr) { 
-    return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
+    return arr.map(({ webformatURL,largeImageURL, tags, likes, views, comments, downloads }) => `
     <div class="photo-card">
+    <a href="${largeImageURL}">
 <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+</a>
 <div class="info">
 <p><b>Likes: </b>${likes}</p>
 <p><b>Views: </b>${views}</p>

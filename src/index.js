3c2c907ec8385536ac123edda3f0f58f954c774c
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-12-10 15:37:17 +0200

    Update markup and button "Load more"

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
 import './css/styles.css';
 
-const BASE_URL = 'https://pixabay.com/api/'
-const filters = `per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+
 
 const refs = {
-    gallery: document.querySelector('.gallery')
+    gallery: document.querySelector('.gallery'),
+    load:document.querySelector('.load-more')
 }
-console.log(refs.gallery)
+refs.load.addEventListener('click', onLoadMore)
 
-function photosApi(name, page=1) { 
+let page = 1;
+const perPage = 100;
+let totalPage = 0;
+
+
+function photosApi(page=1) { 
     const API_KEY = '31885081-e3ce08364707c8044635d8ba7'
-    
-        return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&page=${page}&${filters}`
-    ).then(resp => { 
-        if (!resp.ok) { 
+    const BASE_URL = 'https://pixabay.com/api/'
+    const filters = `per_page=100&image_type=photo&orientation=horizontal&safesearch=true`
+     return fetch(`${BASE_URL}?key=${API_KEY}&q=cat&page=${page}&${filters}`).then(resp => { 
+        if (!resp.ok){ 
             throw new Error()
         }
-        return resp.json()
+        return resp.json('')
     })
 }
-photosApi("cat").then(photo => { 
-    refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(photo))
-})
+
+photosApi().then(data => { 
+refs.gallery.insertAdjacentHTML('beforeend',creatMarkup(data.hits))
+}).catch(err=>console.log(err))
 
 function creatMarkup(arr) { 
-return arr.map(({webformatURL,
-        largeImageURL,
-        tags,likes,views,comments,downloads,
-}) => 
-      `<div class="photo-card">
-     
-      <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
-    
-      <div class="info">
-        <p class="info-item"><b>Likes: </b>${likes}</p>
-        <p class="info-item"><b>Views: </b>${views}</p>
-        <p class="info-item"><b>Comments: </b>${comments}</p>
-        <p class="info-item"><b>Downloads: </b>${downloads}</p>
-      </div>
-    </div>`
-    )
-    .join('');
-    
+    return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
+    <div class="photo-card">
+<img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+<div class="info">
+<p><b>Likes: </b>${likes}</p>
+<p><b>Views: </b>${views}</p>
+<p><b>Comments: </b>${comments}</p>
+ <p><b>Downloads: </b>${downloads}</p>
+  </div>
+    </div>`).join('')
+}
+
+function onLoadMore() { 
+    page += 1;
+photosApi(page).then(data => { 
+    refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(data.hits));
+    totalPage = Math.ceil(data.totalHits/perPage);
+    if (page === totalPage) {
+refs.load.setAttribute('hidden',true)
+     }
+}).catch(err=>console.log(err))
 }

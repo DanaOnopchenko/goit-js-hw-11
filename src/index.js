53d482ecccd9e98f0fc94ffc75f6070c5bc097b7
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-12-10 16:29:56 +0200

    Added Infinite Scroll

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './css/styles.css';
 
 const refs = {
     gallery: document.querySelector('.gallery'),
-    load:document.querySelector('.load-more')
+    load: document.querySelector('.load-more'),
+    quard: document.querySelector('.js-guard')
 }
-refs.load.addEventListener('click', onLoadMore)
+// refs.load.addEventListener('click', onLoadMore)
+
+
+
 
 let page = 1;
 const perPage = 100;
 let totalPage = 0;
 
+let options = {
+  root: null,
+  rootMargin: '150px',
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(onLoad, options);
 
 function photosApi(page=1) { 
     const API_KEY = '31885081-e3ce08364707c8044635d8ba7'
@@ -26,8 +37,10 @@ function photosApi(page=1) {
 }
 
 photosApi().then(data => { 
-refs.gallery.insertAdjacentHTML('beforeend',creatMarkup(data.hits))
+    refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(data.hits))
+    observer.observe(refs.quard)
 }).catch(err=>console.log(err))
+
 
 function creatMarkup(arr) { 
     return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
@@ -42,13 +55,58 @@ function creatMarkup(arr) {
     </div>`).join('')
 }
 
-function onLoadMore() { 
-    page += 1;
-photosApi(page).then(data => { 
-    refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(data.hits));
-    totalPage = Math.ceil(data.totalHits/perPage);
-    if (page === totalPage) {
-refs.load.setAttribute('hidden',true)
-     }
-}).catch(err=>console.log(err))
+function onLoad(entries, observer) { 
+    entries.forEach(entry => {
+        if (entry.isIntersecting) { 
+            console.log('I see you');
+            page += 1;
+            photosApi(page).then(data => { 
+                refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(data.hits));
+                totalPage = Math.ceil(data.totalHits/perPage);
+                if (page === totalPage) { 
+observer.unobserve(refs.quard)
+                }  
+            })
+        }
+    });
+    console.log(entries)
 }
+// function photosApi(page=1) { 
+//     const API_KEY = '31885081-e3ce08364707c8044635d8ba7'
+//     const BASE_URL = 'https://pixabay.com/api/'
+//     const filters = `per_page=100&image_type=photo&orientation=horizontal&safesearch=true`
+//      return fetch(`${BASE_URL}?key=${API_KEY}&q=cat&page=${page}&${filters}`).then(resp => { 
+//         if (!resp.ok){ 
+//             throw new Error()
+//         }
+//         return resp.json('')
+//     })
+// }
+
+// photosApi().then(data => { 
+// refs.gallery.insertAdjacentHTML('beforeend',creatMarkup(data.hits))
+// }).catch(err=>console.log(err))
+
+// function creatMarkup(arr) { 
+//     return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
+//     <div class="photo-card">
+// <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+// <div class="info">
+// <p><b>Likes: </b>${likes}</p>
+// <p><b>Views: </b>${views}</p>
+// <p><b>Comments: </b>${comments}</p>
+//  <p><b>Downloads: </b>${downloads}</p>
+//   </div>
+//     </div>`).join('')
+// }
+
+// function onLoadMore() { 
+//     page += 1;
+// photosApi(page).then(data => { 
+//     refs.gallery.insertAdjacentHTML('beforeend', creatMarkup(data.hits));
+//     totalPage = Math.ceil(data.totalHits/perPage);
+//     if (page === totalPage) {
+// refs.load.setAttribute('hidden',true)
+//      }
+// }).catch(err=>console.log(err))
+// }
